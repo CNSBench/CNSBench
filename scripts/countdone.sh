a928d870e12ceb404684550af83e2c00ddff590f
Alex Merenstein <mmerenstein@cs.stonybrook.edu>
2021-01-18 10:42:23 -0500

    Add helper scripts to container image, so they no longer need to be created from ConfigMaps

---
+++ scripts/countdone.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# $1 = number of containers that can still be running
+# (2 for the parser container, 1 for the output container)
+
+echo $1
+echo $POD_NAME
+
+numcontainers=`curl -k -X GET  -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://$KUBERNETES_PORT_443_TCP_ADDR:$KUBERNETES_SERVICE_PORT_HTTPS/api/v1/namespaces/default/pods/$POD_NAME | jq .status.containerStatuses | grep name | wc -l`
+echo "NUM CONTAINERS $numcontainers"
+numneeded=$(( $numcontainers - $1 ))
+echo "NUM NEEDED $numneeded"
+
+d=$(( $numneeded + 1 ))
+while [[ $d -gt $numneeded ]]; do
+  sleep 5
+  d=`curl -k -X GET  -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" https://$KUBERNETES_PORT_443_TCP_ADDR:$KUBERNETES_SERVICE_PORT_HTTPS/api/v1/namespaces/default/pods/$POD_NAME | jq .status.containerStatuses | grep running | wc -l`
+  echo $d
+done
+
+echo "DONE"
